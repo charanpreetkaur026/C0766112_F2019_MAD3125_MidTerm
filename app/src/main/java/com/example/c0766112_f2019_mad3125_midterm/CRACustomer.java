4772289dae0bef8b3d719385b06d102c8b9bd535
charanpreetkaur026 <charanpreetkaur026@gmail.com>
2019-11-30 16:56:20 -0500

    [charan] CRA class

---
+++ app/src/main/java/com/example/c0766112_f2019_mad3125_midterm/CRACustomer.java
@@ -0,0 +1,39 @@
+package com.example.c0766112_f2019_mad3125_midterm;
+
+public class CRACustomer
+{
+ String firstName;
+ String lastName;
+ String fullName;
+
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFullName() {
+        return firstName + lastName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+
+
+
+
+}
--- app/src/main/java/com/example/c0766112_f2019_mad3125_midterm/MainActivity.java
+++ app/src/main/java/com/example/c0766112_f2019_mad3125_midterm/MainActivity.java
@@ -3,9 +3,15 @@ package com.example.c0766112_f2019_mad3125_midterm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView txtTitle;
+    private EditText edtFirstName;
+    private EditText edtLastName;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
